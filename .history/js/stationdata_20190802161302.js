739bb75b0fba87cad2c42c851bb672b5f18d84c3
jz3309 <43050480+jz3309@users.noreply.github.com>
2019-08-02 19:00:07 -0400

    add files

---
+++ .history/js/stationdata_20190802161302.js
@@ -0,0 +1,133 @@
+var stationdata = [{
+        "Id": 1,
+        "stops": "Lincoln St /Rockaway Bl",
+        "Lat": 40.675242,
+        "Lng": -73.807339
+    },
+    {
+        "Id": 2,
+        "stops": "Lincoln St /Foch Bl",
+        "Lat": 40.676677,
+        "Lng": -73.807505
+    },
+    {
+        "Id": 3,
+        "stops": "Lincoln St /116 Av",
+        "Lat": 40.678699,
+        "Lng": -73.807732
+    },
+    {
+        "Id": 4,
+        "stops": "Lincoln St /115 Av",
+        "Lat": 40.680617,
+        "Lng": -73.807801
+    },
+    {
+        "Id": 5,
+        "stops": "Lincoln St /Linden Bl",
+        "Lat": 40.682654,
+        "Lng": -73.807865
+    },
+    {
+        "Id": 6,
+        "stops": "135 St /111 Av",
+        "Lat": 40.684741,
+        "Lng": -73.807937
+    },
+    {
+        "Id": 7,
+        "stops": "135 St /109 Av",
+        "Lat": 40.687049,
+        "Lng": -73.809102
+    },
+    {
+        "Id": 8,
+        "stops": "135 St /107 Av",
+        "Lat": 40.688478,
+        "Lng": -73.80985
+    },
+    {
+        "Id": 9,
+        "stops": "135 St /105 Av",
+        "Lat": 40.69083,
+        "Lng": -73.811099
+    },
+    {
+        "Id": 10,
+        "stops": "Liberty Av /Van Wyck Exp",
+        "Lat": 40.692036,
+        "Lng": -73.810911
+    },
+    {
+        "Id": 11,
+        "stops": "Liberty Av /Remington St",
+        "Lat": 40.692911,
+        "Lng": -73.809123
+    },
+    {
+        "Id": 12,
+        "stops": "Liberty Av /Inwood St",
+        "Lat": 40.693445,
+        "Lng": -73.807946
+    },
+    {
+        "Id": 13,
+        "stops": "Liberty Av /Liverpool St",
+        "Lat": 40.695038,
+        "Lng": -73.805966
+    },
+    {
+        "Id": 14,
+        "stops": "Sutphin Bl /Liberty Av",
+        "Lat": 40.696082,
+        "Lng": -73.804336
+    },
+    {
+        "Id": 15,
+        "stops": "Sutphin Bl /95 Av",
+        "Lat": 40.698616,
+        "Lng": -73.806264
+    },
+    {
+        "Id": 16,
+        "stops": "Sutphin Bl/Archer Av",
+        "Lat": 40.700816,
+        "Lng": -73.80767
+    },
+    {
+        "Id": 17,
+        "stops": "Jamaica Av /148 St",
+        "Lat": 40.701912,
+        "Lng": -73.806436
+    },
+    {
+        "Id": 18,
+        "stops": "Jamaica Av /153 St",
+        "Lat": 40.702824,
+        "Lng": -73.802085
+    },
+    {
+        "Id": 19,
+        "stops": "Jamaica Av /162 St",
+        "Lat": 40.704348,
+        "Lng": -73.798159
+    },
+    {
+        "Id": 20,
+        "stops": "Jamaica Av /165 St",
+        "Lat": 40.705472,
+        "Lng": -73.794851
+    },
+    {
+        "Id": 21,
+        "stops": "168 St /Jamaica Av",
+        "Lat": 40.706612,
+        "Lng": -73.792506
+    },
+    {
+        "Id": 22,
+        "stops": "165 St Term/Mta Bus Stand",
+        "Lat": 40.707241,
+        "Lng": -73.795443
+    }
+]
